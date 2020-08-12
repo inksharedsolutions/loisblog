7bd187a58073504392550d8b65ff77e72eefc866
inksharedsolutions <webdev.inkshared@gmail.com>
2020-08-13 02:22:48 +0800

    changes-8-13-2020

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,80 +1,73 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from 'react'
+import {Link} from "gatsby"
+import Layout from '../components/layout'
+import Banner from '../components/non-front-banner'
+import Nav from '../components/nav'
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
-import Button from "../components/button"
+const BlogPage = (props)=>{
 
-class Blog extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMdx.edges
+
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        <div style={{ margin: "20px 0 40px" }}>
-          {posts.map(({ node }) => {
-            const title = node.frontmatter.title || node.fields.slug
-            return (
-              <div key={node.fields.slug}>
-                <h3
-                  style={{
-                    marginBottom: rhythm(1 / 4),
-                  }}
-                >
-                  <Link
-                    style={{ boxShadow: `none` }}
-                    to={`/blog${node.fields.slug}`}
-                  >
-                    {title}
-                  </Link>
-                </h3>
-                <small>{node.frontmatter.date}</small>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                />
-              </div>
-            )
-          })}
-        </div>
-        <Link to="/">
-          <Button marginTop="85px">Go Home</Button>
-        </Link>
-      </Layout>
-    )
-  }
-}
+        <>
+            <Layout>
+                <Nav pathExt={props.path}/>
+                <Banner   
+                    spanFirst={`blog page`}
+                    contextHeading={`Blog`}/>
+                
+                <div className="container"  id="blog-body-content-list">
+                    <h1 className="heading-section">
+                        Blog Contents
+                    </h1>
 
-export default Blog
+                    <div className="container">
+                        <section className="grid-parent-blog">
+                            <span className="grid-child blog-num">01.</span>
+                            <div className="grid-child blog-body">
 
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
-      }
-    }
-  }
-`
+                                <span className="blog-date">August 11, 2020</span>
+                                <h1>Blog Title</h1>
+                                <p>
+                                    Lois Richman left Palm Beach, Florida and went
+                                    directly to Hollywood, California right after
+                                    graduation from high school where she was hired as
+                                    a columnist and west coast editor for various fan magazines.
+                                </p>
+                                <div className="blog-link">
+                                    <Link to="">
+                                        Read More
+                                    </Link>
+                                </div>
+                            </div>
+                        </section>
+
+                        <section className="grid-parent-blog">
+                            <span className="grid-child blog-num">01.</span>
+                            <div className="grid-child blog-body">
+
+                                <span className="blog-date">August 11, 2020</span>
+                                <h1>Blog Title</h1>
+                                <p>
+                                    Lois Richman left Palm Beach, Florida and went
+                                    directly to Hollywood, California right after
+                                    graduation from high school where she was hired as
+                                    a columnist and west coast editor for various fan magazines.
+                                </p>
+                                <div className="blog-link">
+                                    <Link to="">
+                                        Read More
+                                    </Link>
+                                </div>
+                            </div>
+                        </section>
+
+                    </div>
+                </div>
+
+            </Layout>
+        </>
+    ) 
+}       
+
+export default BlogPage;
